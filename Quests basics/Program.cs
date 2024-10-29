a8a82145aa232938e232abfbb5404942f06248f6
Tomasz.Kubicki <tomasz.kubicki@uczen.zsk.poznan.pl>
2024-10-29 08:42:44 +0100

    Solution one

---
+++ Quests basics/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+
+Car v1 = new Car("Mercedes", "C klasa", "2007");
+v1.DisplayInfo();
+class Car(string brand, string model,string data)
+{
+    public string Brand = brand;
+    public string Model = model;
+    public string Data = data;
+    public void DisplayInfo()
+    {
+        Console.WriteLine(Model);
+        Console.WriteLine(Brand);
+        Console.WriteLine(Data);
+    }
+}
